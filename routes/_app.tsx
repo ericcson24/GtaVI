f78b0b5aad247626fd5c8d065343270464fa0a89
ericcson24 <150656730+ericcson24@users.noreply.github.com>
2025-05-21 22:41:03 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Cocktail</title>
+        <title>Proyecto-GTAVI</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" type="image/png" href="./Rockstar_Games_Logo.png" ></link>
       </head>
